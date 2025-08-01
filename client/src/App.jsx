8f3ef7b8d0be42dc7b1fdbfec28b6ed2fd28736d
avinasdube <avidubey712@gmail.com>
2025-08-01 14:31:01 +0530

    updated app.jsx and created navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
+      <Navbar />
       Welcome to CurlMate!
     </>
-  )
+  );
 }
 
-export default App
+export default App;
